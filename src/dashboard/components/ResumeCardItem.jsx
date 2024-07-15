7a91e2c3c6ab897029bed9cc9c0e40e6fbc3ba78
Divyansh_Sharma <111333281+Divyansh10Sharma@users.noreply.github.com>
2024-07-15 21:07:47 +0530

    Updated dynamic text color on resume card

--- src/dashboard/components/ResumeCardItem.jsx
+++ src/dashboard/components/ResumeCardItem.jsx
@@ -49,7 +49,7 @@ function ResumeCardItem({resume,refreshData}) {
       
         <div className=''>
           <Link to={'/dashboard/resume/'+resume.documentId+"/edit"}>
-            <div className='p-14 bg-gradient-to-b from-pink-100 via-purple-200 to-blue-200 h-[280px] rounded-t-lg border-t-4' style={{borderColor:resume?.themeColor}}>
+            <div className={`border p-3 flex justify-between rounded-b-lg shadow-lg  ${resume?.themeColor ? 'text-white' : 'text-black'}`} style={{background:resume?.themeColor}}>
                   <div className='flex items-center justify-center h-[180px] '>
                     {/* <Notebook/> */}
                     <img src="/cv.png" width={80} height={80} />
