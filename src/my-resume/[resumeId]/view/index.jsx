ae3c8db4c144bf2fb23b3e23f50b1d5e52838ebb
Divyansh_Sharma <111333281+Divyansh10Sharma@users.noreply.github.com>
2024-07-16 19:12:43 +0530

    Update index.jsx

--- src/my-resume/[resumeId]/view/index.jsx
+++ src/my-resume/[resumeId]/view/index.jsx
@@ -34,37 +34,37 @@ function ViewResume() {
     };
 
     return (
-        <div id="no-print">
-            <Header />
-            <ResumeInfoContext.Provider value={{resumeInfo,setResumeInfo}}>
-                <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
-                    <h2 className='text-center text-2xl font-medium'>
-                        Congrats! Your Ultimate AI generated Resume is ready!
-                    </h2>
-                    <p className='text-center text-gray-400'>
-                        Now you are ready to download your resume and you can share the unique resume URL with your friends and family
-                    </p>
-                    <div className='flex justify-between px-44 my-10'>
-                        <Button onClick={HandleDownload}>Download</Button>
-                        <RWebShare
-                            data={{
-                                text: "Hello Everyone, This is my resume please open URL to see it",
-                                url: `${import.meta.env.VITE_API_BASE_URL}/my-resume/${resumeId}/view`,
-                                title: `${resumeInfo?.firstName} ${resumeInfo?.lastName} resume`,
-                            }}
-                            onClick={() => console.log("shared successfully!")}
-                        >
-                            <Button>Share</Button>
-                        </RWebShare>
+        <ResumeInfoContext.Provider value={{resumeInfo,setResumeInfo}}>
+            <div id="no-print">
+                <Header />
+                    <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
+                        <h2 className='text-center text-2xl font-medium'>
+                            Congrats! Your Ultimate AI generated Resume is ready!
+                        </h2>
+                        <p className='text-center text-gray-400'>
+                            Now you are ready to download your resume and you can share the unique resume URL with your friends and family
+                        </p>
+                        <div className='flex justify-between px-44 my-10'>
+                            <Button onClick={HandleDownload}>Download</Button>
+                            <RWebShare
+                                data={{
+                                    text: "Hello Everyone, This is my resume please open URL to see it",
+                                    url: `${import.meta.env.VITE_API_BASE_URL}/my-resume/${resumeId}/view`,
+                                    title: `${resumeInfo?.firstName} ${resumeInfo?.lastName} resume`,
+                                }}
+                                onClick={() => console.log("shared successfully!")}
+                            >
+                                <Button>Share</Button>
+                            </RWebShare>
+                        </div>
                     </div>
-                </div>
-                <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
-                    <div id="print-area">
-                        <ResumePreview />
+            </div>
+            <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
+                        <div id="print-area">
+                            <ResumePreview />
+                        </div>
                     </div>
-                </div>
-            </ResumeInfoContext.Provider>
-        </div>
+        </ResumeInfoContext.Provider>
     );
 }
 
