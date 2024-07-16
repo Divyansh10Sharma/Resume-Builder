1c5ea1914b6e01a9218f7b42995ccad290398df0
Divyansh_Sharma <111333281+Divyansh10Sharma@users.noreply.github.com>
2024-07-16 18:56:57 +0530

    Update ResumeInfoContext.jsx

--- src/context/ResumeInfoContext.jsx
+++ src/context/ResumeInfoContext.jsx
@@ -1,3 +1,13 @@
-import { createContext } from "react";
+import React, { createContext, useState } from 'react';
 
-export const ResumeInfoContext=createContext(null);
+export const ResumeInfoContext = createContext();
+
+export const ResumeInfoProvider = ({ children }) => {
+    const [resumeInfo, setResumeInfo] = useState(null);
+
+    return (
+        <ResumeInfoContext.Provider value={{ resumeInfo, setResumeInfo }}>
+            {children}
+        </ResumeInfoContext.Provider>
+    );
+};
