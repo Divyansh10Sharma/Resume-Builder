3511479bda68836cd437b630cbf2fd179b6ae727
deveshsharmaontech <deveshsharmaontech@gmail.com>
2025-05-06 00:23:22 +0530

    jujujujujuj

--- src/my-resume/[resumeId]/view/index.jsx
+++ src/my-resume/[resumeId]/view/index.jsx
@@ -45,11 +45,11 @@ function ViewResume() {
 
     const renderTemplate = (templateId) => {
         switch (templateId) {
-            case 1: return <Template1 resumeInfo={resumeInfo} ref={printRef} />;
-            case 2: return <Template2 resumeInfo={resumeInfo} ref={printRef} />;
-            case 3: return <Template3 resumeInfo={resumeInfo} ref={printRef} />;
-            case 4: return <Template4 resumeInfo={resumeInfo} ref={printRef} />;
-            case 4: return <Template5 resumeInfo={resumeInfo} ref={printRef} />;
+            case 'template1': return <Template1 resumeInfo={resumeInfo} ref={printRef} />;
+            case 'template2': return <Template2 resumeInfo={resumeInfo} ref={printRef} />;
+            case 'template3': return <Template3 resumeInfo={resumeInfo} ref={printRef} />;
+            case 'template4': return <Template4 resumeInfo={resumeInfo} ref={printRef} />;
+            case 'template4': return <Template5 resumeInfo={resumeInfo} ref={printRef} />;
             default: return <Template1 resumeInfo={resumeInfo} ref={printRef} />;
         }
     };
