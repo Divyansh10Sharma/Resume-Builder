c43b9a176c425122ed0c3ad2ece0e4d457cda4b9
deveshsharmaontech <deveshsharmaontech@gmail.com>
2025-05-05 22:28:28 +0530

    kakakaka

--- src/dashboard/resume/[resumeId]/edit/index.jsx
+++ src/dashboard/resume/[resumeId]/edit/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import FormSection from '../../components/FormSection';
 import ResumePreview from '../../components/ResumePreview';
